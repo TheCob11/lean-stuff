efca3df3906fec8a6906675d2c789594313470d3
TheCob11 <adipo12345678@gmail.com>
2025-08-18 20:10:49 -0400

    more work on burnside

--- Misc/BurnsideLemma.lean
+++ Misc/BurnsideLemma.lean
@@ -11,20 +11,34 @@ def sigmaFixedEquivSigmaStab : (g: G) × fixedBy X g ≃ (x: X) × stabilizer G 
 
 -- #check orbitEquivQuotientStabilizer G ?x |>.symm
 -- #check orbitProd
-#check selfEquivSigmaOrbits' G X
+-- #check selfEquivSigmaOrbits' G X
 
+example [Fintype G] [Fintype X] : ∑g: G, Nat.card (fixedBy X g) =
+                      Nat.card (orbitRel.Quotient G X) * Nat.card G := calc
+  ∑g: G, Nat.card (fixedBy X g)
+  _ = ∑x: X, Nat.card (stabilizer G x) := by
+    refine
+      Function.Bijective.finset_sum ?e ?he (fun x ↦ Nat.card ↑(fixedBy X x))
+        (fun x ↦ Nat.card ↥(stabilizer G x)) ?h
+    sorry
+    sorry
+    sorry
+  _ = _ := sorry
 
 noncomputable
 example : (x: X) × stabilizer G x ≃ G × orbitRel.Quotient G X where
   -- toFun := fun ⟨x, ⟨g, hg⟩⟩ ↦ (groupEquivQuotientProdSubgroup.symm (g, ⟨g, hg⟩), ⟦x⟧)
-  toFun := fun ⟨x, ⟨g, hg⟩⟩ ↦ ⟨g, selfEquivSigmaOrbits' G X x |>.fst⟩
-  invFun := fun (g, qx) ↦
-    ⟨selfEquivSigmaOrbits' G X|>.symm ⟨qx, sorry⟩, groupEquivQuotientProdSubgroup g |>.snd⟩
-  -- invFun := fun (g, qx) ↦ ⟨qx.out'⟩
-  left_inv := fun ⟨x, ⟨g, hg⟩⟩ ↦ by
+  toFun := fun ⟨x, ⟨g, hg⟩⟩ ↦ (g, selfEquivSigmaOrbits' G X x |>.fst)
+  -- invFun := fun (g, qx) ↦
+  --   ⟨selfEquivSigmaOrbits' G X|>.symm ⟨qx, ⟨qx.out', sorry⟩⟩, groupEquivQuotientProdSubgroup g |>.snd⟩
+  invFun := fun (g, qx) ↦ ⟨qx.out', groupEquivQuotientProdSubgroup g |>.snd⟩
+  left_inv := fun ⟨x, ⟨g, (hg: g • x = x)⟩⟩ ↦ by
     simp only [Sigma.mk.inj_iff]
-
-    sorry
+    apply And.intro
+    ·
+      sorry
+    ·
+      sorry
   right_inv := fun ⟨g, qx⟩ ↦ by
     -- refine Prod.ext ?_ (Quotient.out_eq' qx)
     -- simp only [Subtype.coe_eta]
